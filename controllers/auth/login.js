8ea54b78c505a70e7fcb22e5f3ef02e62a772424
Marina-93 <marina.ua93@gmail.com>
2022-04-04 01:19:30 +0300

    authControllers

---
+++ controllers/auth/login.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { User, schems } = require('../../models/user');
+
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { error } = schems.registet.validate(req.body);
+
+  if (error) {
+    throw new createError(400, error.message);
+  }
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new createError(401, 'Email or password is wrong');
+  }
+
+  const compareResult = await bcrypt.compare(password, user.password);
+
+  if (!compareResult) {
+    throw new createError(401, 'Email or password is wrong');
+  }
+
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '5h' });
+
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    user: {
+      token,
+      email,
+    },
+  });
+};
+
+module.exports = login;
