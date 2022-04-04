1e5e780419d0cf72776599ca120ff09baf7d17e6
dkolot <dkolotsoftserveinc.com>
2022-04-04 21:35:19 +0300

    swagger: add swagger

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-// const swaggerUi = require('swagger-ui-express');
-
-// const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const authRouter = require('./routes/api/auth');
-//colection
 
 const app = express();
 
@@ -16,10 +14,16 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    
+const options = {
+  explorer: true,
+};
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 app.use('/api/users', authRouter);
-//colection
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
