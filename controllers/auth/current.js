7bc2a48cb9235b56bc28870f93700fe5a8db9feb
dkolot <dkolotsoftserveinc.com>
2022-04-07 20:14:47 +0300

    user-avatar: add token into get curren user endpoint

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,7 +1,7 @@
 const current = async (req, res) => {
-  const { email, avatarURL } = req.user;
+  const { email, avatarURL, token } = req.user;
 
-  res.json({ email, avatarURL });
+  res.json({ email, avatarURL, token });
 };
 
 module.exports = current;
