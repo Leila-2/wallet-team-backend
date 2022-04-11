54c60cada03c6b5089400edd001ae55df4384994
dkolot <dkolotsoftserveinc.com>
2022-04-11 22:34:29 +0300

    category-update

--- controllers/transactions/statistics.js
+++ controllers/transactions/statistics.js
@@ -1,4 +1,4 @@
-const { Transaction } = require("../../models/transaction");
+const { Transaction } = require('../../models/transaction');
 
 const all = async (req, res) => {
   const { month, year } = req.query;
@@ -21,37 +21,61 @@ const all = async (req, res) => {
     leisure: 0,
     other: 0,
   };
+
   const categories = [
-    "main",
-    "food",
-    "car",
-    "me",
-    "children",
-    "house",
-    "education",
-    "leisure",
-    "other",
+    {
+      title: 'Основной',
+      value: 'main',
+    },
+    {
+      title: 'Еда',
+      value: 'food',
+    },
+    {
+      title: 'Авто',
+      value: 'car',
+    },
+    {
+      title: 'Развитие',
+      value: 'me',
+    },
+    {
+      title: 'Дети',
+      value: 'children',
+    },
+    {
+      title: 'Дом',
+      value: 'house',
+    },
+    {
+      title: 'Образование',
+      value: 'education',
+    },
+    {
+      title: 'Остальные',
+      value: 'other',
+    },
   ];
 
   categories.forEach((categoryExp) => {
     transactions.forEach((item) => {
-      if (item.category === categoryExp) {
-        sumCategories[categoryExp] += item.amount;
+      if (item.category === categoryExp.value) {
+        sumCategories[categoryExp.value] += item.amount;
       }
     });
   });
 
   transactions.forEach((item) => {
-    if (item.type === "incomes") {
+    if (item.type === 'incomes') {
       incomes += item.amount;
     }
-    if (item.type === "expenses") {
+    if (item.type === 'expenses') {
       expenses += item.amount;
     }
   });
 
   return res.status(200).json({
-    status: "success",
+    status: 'success',
     code: 200,
     transactions: sumCategories,
     expenses: expenses,
